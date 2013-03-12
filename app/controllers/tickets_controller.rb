55d15b8e626f82b6cb0737db9a7bef0ef9364abd
Steve Klabnik <steve@steveklabnik.com>
2013-03-11 23:04:23 -0700

    Implement delete tickets feature

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -36,6 +36,13 @@ class TicketsController < ApplicationController
     end
   end
 
+  def destroy
+    @ticket.destroy
+    flash[:notice] = "Ticket has been deleted."
+
+    redirect_to @project
+  end
+
   private 
     def set_project
       @project = Project.find(params[:project_id])
