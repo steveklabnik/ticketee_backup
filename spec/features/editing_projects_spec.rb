8dd717ca2f91674a061e43a61d52c64da23b562b
Steve Klabnik <steve@steveklabnik.com>
2013-04-04 15:12:02 -0500

    I missed two #should's

--- spec/features/editing_projects_spec.rb
+++ spec/features/editing_projects_spec.rb
@@ -13,14 +13,14 @@ feature "Editing Projects" do
     fill_in "Name", with: "TextMate 2 beta"
     click_button "Update Project"
 
-    page.should have_content("Project has been updated.")
+    expect(page).to have_content("Project has been updated.")
   end
 
   scenario "Updating a project with invalid attributes is bad" do
     fill_in "Name", with: ""
     click_button "Update Project"
 
-    page.should have_content("Project has not been updated.")
+    expect(page).to have_content("Project has not been updated.")
   end
 end
 
